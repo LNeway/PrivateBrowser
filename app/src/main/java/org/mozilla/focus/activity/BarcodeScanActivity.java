0c700c44af267b3c3a9f8cd4e0f1cd8fa7e4b209
liuwei64 <liuwei64@baidu.com>
2018-01-10 13:19:12 +0800

    add camera support.

--- app/src/main/java/org/mozilla/focus/activity/BarcodeScanActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/BarcodeScanActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.google.android.cameraview.CameraView;
+
 import org.mozilla.focus.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -16,11 +19,21 @@ import butterknife.OnClick;
 
 public class BarcodeScanActivity extends Activity {
 
+    @BindView(R.id.cameraView)
+    CameraView cameraView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
         ButterKnife.bind(this);
+        cameraView.addCallback(new CameraView.Callback() {
+            @Override
+            public void onPictureTaken(CameraView cameraView, byte[] data) {
+
+            }
+        });
+
     }
 
     @OnClick({R.id.backImg, R.id.galleryImg})
@@ -30,9 +43,20 @@ public class BarcodeScanActivity extends Activity {
                 finish();
                 break;
             case R.id.galleryImg:
-
                 break;
             default:
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
     }
 }
